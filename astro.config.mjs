266b4251c22b88fcb41a23a771300d27a309d528
srwebcl <contacto@srweb.cl>
2025-12-11 04:20:32 -0300

    actualización completa y entorno listo para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({}),
   integrations: [react()]
 });
